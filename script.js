ee4e1e92e527c3d6e8090ba1c5fe4ec83f3b952a
Charlie Sumalag <charliesumalag1991@gmail.com>
2024-05-30 13:54:10 +0800

    initialize commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const desTextEl = document.getElementById("des-text");
+
+const desTextArray = desTextEl.innerText;
+console.log(desTextArray);
+
+for (let i = 0; i < desTextArray.length; i++) {
+  console.log(desTextArray[i]);
+}
